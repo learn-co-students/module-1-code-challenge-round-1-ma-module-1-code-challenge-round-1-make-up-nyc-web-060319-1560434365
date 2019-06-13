d338928998327824f12d516f84a7d3f15bacef59
github username <github email address>
2019-06-13 11:03:43 -0400

    Code Challenge

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,20 @@
 class Review
-  
+
+@@all = []
+
+def initialize(restaurant, reviews)
+    @Restaurant = restaurant
+    @Reviews = reviews
+    @@all << self
 end
+
+def self.all
+    @@all
+end
+
+#cdef self.all
+#    get_it.select do |self|
+        self.all
+
+
 
--- tools/console.rb
+++ tools/console.rb
@@ -8,4 +8,4 @@ end
 # so they will be available to test and play around with in your console
 
 binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+#leave this here to ensure binding.pry isn't the last line
